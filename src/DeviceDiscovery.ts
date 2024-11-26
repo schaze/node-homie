89430691178c818828f46b9038ba7edec1e60284
schaze <thomas_schaz@gmx.de>
2024-11-26 20:11:04 +0100

    updates

--- src/DeviceDiscovery.ts
+++ src/DeviceDiscovery.ts
@@ -7,28 +7,27 @@ import { bufferTime, concatMap, delay, delayWhen, filter, mergeMap, takeUntil } 
 import { SimpleLogger } from "./misc/Logger";
 import { makeV5BaseTopic } from "./util";
 
-export type DiscoveryEventType = 'add' | 'remove' | 'error';
+export type DiscoveryEventType = "add" | "remove" | "error";
 
 export interface BaseDiscoveryEvent<T extends DiscoveryEventType> {
     type: T;
 }
 
-export interface AddDiscoveryEvent extends BaseDiscoveryEvent<'add'> {
+export interface AddDiscoveryEvent extends BaseDiscoveryEvent<"add"> {
     deviceId: HomieID;
     retained: boolean;
     makeDevice: () => HomieDevice;
 }
 
-export interface RemoveDiscoveryEvent extends BaseDiscoveryEvent<'remove'> {
+export interface RemoveDiscoveryEvent extends BaseDiscoveryEvent<"remove"> {
     deviceId: HomieID;
 }
 
-export interface ErrorDiscoveryEvent extends BaseDiscoveryEvent<'error'> {
+export interface ErrorDiscoveryEvent extends BaseDiscoveryEvent<"error"> {
     error?: Error;
 }
 
 export type DiscoveryEvent = AddDiscoveryEvent | RemoveDiscoveryEvent | ErrorDiscoveryEvent;
-
 
 const BUFFERTIME = 250;
 const BUFFERSIZE = 10;
@@ -42,17 +41,16 @@ export class DeviceDiscovery extends LifecycleBase {
 
     protected _discoveryEvents$ = new Subject<Observable<DiscoveryEvent>>();
 
-
     constructor(
         protected mqttOpts: MQTTConnectOpts,
         protected sharedMQTTClient?: RxMqtt,
         protected nonRetainedDiscoveryDelay = NONREATAINEDDELAY,
         protected rateLimit = RATELIMIT,
         protected bufferTimeLimit = BUFFERTIME,
-        protected bufferSize = BUFFERSIZE
+        protected bufferSize = BUFFERSIZE,
     ) {
         super();
-        this.log = new SimpleLogger(this.constructor.name, 'discovery');
+        this.log = new SimpleLogger(this.constructor.name, "discovery");
 
         const stateTopic = `${makeV5BaseTopic(this.mqttOpts.topicRoot)}/+/${HD_ATTR_STATE}`;
         this.mqtt = new RxMqtt(this.mqttOpts);
@@ -63,17 +61,24 @@ export class DeviceDiscovery extends LifecycleBase {
         // @ts-ignore:
         bufferTime(this.bufferTimeLimit, undefined, this.bufferSize),
         // do nothing on empty buffers
-        filter(events => events.length > 0),
+        filter((events) => events.length > 0),
         // apply at least RATELIMIT delay on new events
-        // @ts-ignore: 
-        concatMap(events => of(events).pipe(delay(this.rateLimit))),
+        // @ts-ignore:
+        concatMap((events) => of(events).pipe(delay(this.rateLimit))),
         // merge buffer into single event observable stream
-        mergeMap(events => events),
+        mergeMap((events) => events),
         // merge down observable of event to event objects, apply delay for non-retained and remove messages
-        // @ts-ignore: 
-        mergeMap(event => event.pipe(delayWhen(event => (event.type === 'add' && !event.retained) || event.type === 'remove' ? timer(this.nonRetainedDiscoveryDelay) : timer(0))))
+        // @ts-ignore:
+        mergeMap((event) =>
+            event.pipe(
+                delayWhen((event) =>
+                    (event.type === "add" && !event.retained) || event.type === "remove"
+                        ? timer(this.nonRetainedDiscoveryDelay)
+                        : timer(0),
+                ),
+            ),
+        ),
     );
-
 
     public override async onInit() {
         if (this.sharedMQTTClient) {
@@ -83,37 +88,42 @@ export class DeviceDiscovery extends LifecycleBase {
 
         const rootTopic = makeV5BaseTopic(this.mqttOpts.topicRoot);
 
-        this.mqtt.subscribe(`${rootTopic}/+/${HD_ATTR_STATE}`, true).pipe(takeUntil(this.onDestroy$)).subscribe({
-            next: msg => {
-                const { topic, payload, packet } = msg;
+        this.mqtt
+            .subscribe(`${rootTopic}/+/${HD_ATTR_STATE}`, true)
+            .pipe(takeUntil(this.onDestroy$))
+            .subscribe({
+                next: (msg) => {
+                    const { topic, payload, packet } = msg;
 
-                const relTopic = topic.split(rootTopic + '/')[1];
-                const [deviceId] = relTopic.split('/');
+                    const relTopic = topic.split(rootTopic + "/")[1];
+                    const [deviceId] = relTopic.split("/");
 
-                if (payload.length === 0) { // device removal
-                    this.nextEvent(<RemoveDiscoveryEvent>{ type: 'remove', deviceId: deviceId });
-                    return;
-                }
-                this.nextEvent(<AddDiscoveryEvent>{
-                    type: 'add', retained: packet.retain, deviceId,
-                    makeDevice: () => this.makeDevice(deviceId)
-                })
-            },
-            error: (err) => {
-                this.log.error('Error in discovery loop. No new devices will be discovered: ', { error: err });
-                this.nextEvent(<ErrorDiscoveryEvent>{ type: 'error', error: err });
-            }
-        });
-
+                    if (payload.length === 0) {
+                        // device removal
+                        this.nextEvent(<RemoveDiscoveryEvent>{ type: "remove", deviceId: deviceId });
+                        return;
+                    }
+                    this.nextEvent(<AddDiscoveryEvent>{
+                        type: "add",
+                        retained: packet.retain,
+                        deviceId,
+                        makeDevice: () => this.makeDevice(deviceId),
+                    });
+                },
+                error: (err) => {
+                    this.log.error("Error in discovery loop. No new devices will be discovered: ", { error: err });
+                    this.nextEvent(<ErrorDiscoveryEvent>{ type: "error", error: err });
+                },
+            });
     }
 
     public override async onDestroy() {
         try {
             await super.onDestroy();
             await this.mqtt.onDestroy();
-            this.log.debug('mqtt connecition closed.');
+            this.log.debug("mqtt connecition closed.");
         } catch (err) {
-            this.log.error('Error closing mqtt connection: ', err);
+            this.log.error("Error closing mqtt connection: ", err);
         }
     }
 
@@ -121,10 +131,11 @@ export class DeviceDiscovery extends LifecycleBase {
         this._discoveryEvents$.next(of(event));
     }
 
-
     public makeDevice(id: HomieID): HomieDevice {
-        return new HomieDevice({ id }, this.sharedMQTTClient ? this.sharedMQTTClient : this.mqttOpts, HomieDeviceMode.Controller);
+        return new HomieDevice(
+            { id },
+            this.sharedMQTTClient ? this.sharedMQTTClient : this.mqttOpts,
+            HomieDeviceMode.Controller,
+        );
     }
-
-
 }
--- src/rx/Operators.ts
+++ src/rx/Operators.ts
@@ -1,72 +1,61 @@
 import { combineLatest, Observable, of, OperatorFunction } from "rxjs";
-import { distinctUntilChanged, filter, map, mapTo, pluck, switchMap, take, takeWhile, tap } from "rxjs/operators";
+import { distinctUntilChanged, filter, map, mapTo, switchMap } from "rxjs/operators";
 import { notNull, notNullish, notUndefined } from "../model";
 
-
-
 export function isNotNullish<T>() {
-    return (source$: Observable<null | undefined | T>) =>
-        source$.pipe(
-            filter(notNullish)
-        );
+    return (source$: Observable<null | undefined | T>) => source$.pipe(filter(notNullish));
 }
 
 export function isNotNull<T>() {
-    return (source$: Observable<null | undefined | T>) =>
-        source$.pipe(
-            filter(notNull)
-        );
+    return (source$: Observable<null | undefined | T>) => source$.pipe(filter(notNull));
 }
 
 export function isNotUndefined<T>() {
-    return (source$: Observable<null | undefined | T>) =>
-        source$.pipe(
-            filter(notUndefined)
-        );
+    return (source$: Observable<null | undefined | T>) => source$.pipe(filter(notUndefined));
 }
 
 export function watchList<T, R>(select: (item: T) => Observable<R>): OperatorFunction<T[], T[]> {
     return (source: Observable<T[]>): Observable<T[]> => {
         return source.pipe(
-            switchMap(items => (!items || items.length === 0) ? of<T[]>([]) :
-                combineLatest(items.map(item => select(item).pipe(mapTo(item))))
-            )
-        )
-    }
-
+            switchMap((items) =>
+                !items || items.length === 0
+                    ? of<T[]>([])
+                    : combineLatest(items.map((item) => select(item).pipe(mapTo(item)))),
+            ),
+        );
+    };
 }
 
 export function mergeWatchList<T, S>(predicate: (item: T) => Observable<S[]>): OperatorFunction<T[], S[]> {
-    return (source: Observable<T[]>): Observable<S[]> => source.pipe(
-        switchMap(list => (!list || list.length === 0) ? of<S[]>([]) :
-            combineLatest(
-                list.map(item => predicate(item))
-            ).pipe(
-                map(listOfLists => listOfLists.flat()),
-            )
-        )
-    )
+    return (source: Observable<T[]>): Observable<S[]> =>
+        source.pipe(
+            switchMap((list) =>
+                !list || list.length === 0
+                    ? of<S[]>([])
+                    : combineLatest(list.map((item) => predicate(item))).pipe(map((listOfLists) => listOfLists.flat())),
+            ),
+        );
 }
 
-export function mergeWatchIndex<T, S>(predicate: (item: T) => Observable<{ [pointer: string]: S }>): OperatorFunction<T[], { [pointer: string]: S }> {
-    return (source: Observable<T[]>): Observable<{ [pointer: string]: S }> => source.pipe(
-        switchMap(list => (!list || list.length === 0) ? of<{ [pointer: string]: S }>({}) :
-            combineLatest(
-                list.map(item => predicate(item))
-            ).pipe(
-                map(indexList => mergeIndex(indexList))
-            )
-        )
-    )
+export function mergeWatchIndex<T, S>(
+    predicate: (item: T) => Observable<{ [pointer: string]: S }>,
+): OperatorFunction<T[], { [pointer: string]: S }> {
+    return (source: Observable<T[]>): Observable<{ [pointer: string]: S }> =>
+        source.pipe(
+            switchMap((list) =>
+                !list || list.length === 0
+                    ? of<{ [pointer: string]: S }>({})
+                    : combineLatest(list.map((item) => predicate(item))).pipe(
+                          map((indexList) => mergeIndex(indexList)),
+                      ),
+            ),
+        );
 }
 
 export function toIndex<T>(getIndex: (item: T) => string): OperatorFunction<T[], { [pointer: string]: T }> {
-    return (source: Observable<T[]>): Observable<{ [pointer: string]: T }> => source.pipe(
-        map(items => makeIndex(items, getIndex))
-    )
+    return (source: Observable<T[]>): Observable<{ [pointer: string]: T }> =>
+        source.pipe(map((items) => makeIndex(items, getIndex)));
 }
-
-
 
 export function makeIndex<T>(itemList: T[], getIndex: (item: T) => string): { [pointer: string]: T } {
     const propsIndexed: { [pointer: string]: T } = {};
@@ -77,94 +66,88 @@ export function makeIndex<T>(itemList: T[], getIndex: (item: T) => string): { [p
     return propsIndexed;
 }
 
-
-
 export function mergeIndex<T>(indexes: { [pointer: string]: T }[]): { [pointer: string]: T } {
     var newIndex: { [pointer: string]: T } = {};
     for (let index = 0; index < indexes.length; index++) {
         const nodeIndex = indexes[index];
         newIndex = { ...newIndex, ...nodeIndex };
     }
-    return newIndex
+    return newIndex;
 }
 
-
-
-
-export function mandatoryPluckSwitchMap<T, R, K extends keyof T>(attr: K, project: (v: T[K]) => Observable<R> | null | undefined, distinct = true): OperatorFunction<T, R> {
+export function mandatoryPluckSwitchMap<T, R, K extends keyof T>(
+    attr: K,
+    project: (v: T[K]) => Observable<R> | null | undefined,
+    distinct = true,
+): OperatorFunction<T, R> {
     return (source: Observable<T>): Observable<R> => {
         // pluck <attr> from object, only emit if changed and switch subscription to projected observable
         // logic is, that if our mandatory pluck emits a value we found our target and stop listening to the source observable
-        const resObs = source.pipe(mandatoryPluck(attr), distinctUntilChanged(), mandatorySwitchMap(project))
+        const resObs = source.pipe(mandatoryPluck(attr), distinctUntilChanged(), mandatorySwitchMap(project));
         return distinct ? resObs.pipe(distinctUntilChanged()) : resObs;
-    }
+    };
 }
 
-export function optionalPluckSwitchMap<T, R, K extends keyof T>(attr: K, project: (v: T[K] | null | undefined) => Observable<R> | null | undefined, distinct = true): OperatorFunction<T | undefined, R | undefined> {
+export function optionalPluckSwitchMap<T, R, K extends keyof T>(
+    attr: K,
+    project: (v: T[K] | null | undefined) => Observable<R> | null | undefined,
+    distinct = true,
+): OperatorFunction<T | undefined, R | undefined> {
     return (source: Observable<T | undefined>): Observable<R | undefined> => {
         // pluck <attr> from source object emissions and only emit if changed
         // when the result is here switch subscription to projected observable
-        const resObs = source.pipe(optionalPluck(attr), distinctUntilChanged(), optionalSwitchMap(project))
+        const resObs = source.pipe(optionalPluck(attr), distinctUntilChanged(), optionalSwitchMap(project));
         return distinct ? resObs.pipe(distinctUntilChanged()) : resObs;
-    }
+    };
 }
-
 
 export function mandatorySwitchMap<T, R>(project: (v: T) => Observable<R> | null | undefined): OperatorFunction<T, R> {
     return (source: Observable<T>): Observable<R> => {
         return source.pipe(
-            switchMap(v => project(v) ?? of(undefined)),
-            mandatory()
+            switchMap((v) => project(v) ?? of(undefined)),
+            mandatory(),
         );
-
-    }
+    };
 }
 
-export function optionalSwitchMap<T, R>(project: (v: T | null | undefined) => Observable<R> | null | undefined): OperatorFunction<T | undefined, R | undefined> {
+export function optionalSwitchMap<T, R>(
+    project: (v: T | null | undefined) => Observable<R> | null | undefined,
+): OperatorFunction<T | undefined, R | undefined> {
     return (source: Observable<T | undefined>): Observable<R | undefined> => {
         return source.pipe(
-            switchMap(v => {
+            switchMap((v) => {
                 const projRes = project(v);
-                return (projRes !== undefined && projRes !== null) ? projRes : of(undefined)
-            })
+                return projRes !== undefined && projRes !== null ? projRes : of(undefined);
+            }),
         );
-
-    }
+    };
 }
 
 export function mandatoryPluck<T, K extends keyof T>(attr: K): OperatorFunction<T, T[K]> {
     return (source: Observable<T>): Observable<T[K]> => {
         return source.pipe(
-            map(attrs => attrs[attr]),
-            mandatory()
+            map((attrs) => attrs[attr]),
+            mandatory(),
         );
-
-    }
+    };
 }
 
 export function optionalPluck<T, K extends keyof T>(attr: K): OperatorFunction<T | undefined, T[K] | undefined> {
     return (source: Observable<T | undefined>): Observable<T[K] | undefined> => {
-        return source.pipe(
-            map(v => v ? v[attr] : undefined)
-        );
-
-    }
+        return source.pipe(map((v) => (v ? v[attr] : undefined)));
+    };
 }
-
 
 export function mandatory<T, R extends T>(): OperatorFunction<T | null | undefined, R> {
     return (source: Observable<T | null | undefined>): Observable<R> => {
-        return source.pipe(
-            filter(v => v !== null && v !== undefined),
-        ) as Observable<R>;
-    }
+        return source.pipe(filter((v) => v !== null && v !== undefined)) as Observable<R>;
+    };
 }
-
 
 export function flattenAndRemoveNullish<T, R extends NonNullable<T>>(): OperatorFunction<T[][], R[]> {
     return (source: Observable<T[][]>): Observable<R[]> => {
         return source.pipe(
-            map(objs => {
+            map((objs) => {
                 const filtered = [];
                 for (let index = 0; index < objs.length; index++) {
                     const dataSet = objs[index];
@@ -176,17 +159,15 @@ export function flattenAndRemoveNullish<T, R extends NonNullable<T>>(): Operator
                     }
                 }
                 return filtered;
-            })
-
+            }),
         ) as Observable<R[]>;
-    }
+    };
 }
-
 
 export function removeNullish<T, R extends NonNullable<T>>(): OperatorFunction<T[], R[]> {
     return (source: Observable<T[]>): Observable<R[]> => {
         return source.pipe(
-            map(objs => {
+            map((objs) => {
                 const filtered = [];
                 for (let index = 0; index < objs.length; index++) {
                     const node = objs[index];
@@ -195,8 +176,8 @@ export function removeNullish<T, R extends NonNullable<T>>(): OperatorFunction<T
                     }
                 }
                 return filtered;
-            })
-
+            }),
         ) as Observable<R[]>;
-    }
+    };
 }
+
